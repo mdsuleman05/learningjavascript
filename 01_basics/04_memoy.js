39bf8cb82c8379af91cb56699e18a1d9408a8ab1
mdsuleman05 <165216762+mdsuleman05@users.noreply.github.com>
2024-03-28 09:33:35 +0000

    stack and heap

---
+++ 01_basics/04_memoy.js
@@ -0,0 +1,13 @@
+// ********stack memory(ptimitive),heap(non-primitive)********
+let myName="suleman"
+let anotherName=myName;
+anotherName="shamoon"
+
+console.log(myName);
+console.log(anotherName)
+
+let user={
+    email:mdsuleman,
+    upi:www@ybl,
+    
+}
