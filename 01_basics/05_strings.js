7330d8dee6c99265b6bc8b2cb429a972fa47aa7c
mdsuleman05 <165216762+mdsuleman05@users.noreply.github.com>
2024-03-28 12:40:00 +0000

    string methods

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -4,3 +4,13 @@ console.log(name+ repoCount+"value");
 console.log(`hello my name is ${name} and my repocount is ${repoCount}`)
 
 const gameName = new String(`suleman`)
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf('n'));
+let newString=gameName.substring(0,4);//last number is not included
+console.log(newString);
+const anotherString=gameName.slice(-6,4);
+console.log(anotherString)
+const url="shamoon_suleman"
+console.log(url.replace(`_`,`&`));
+console.log(gameName.split(`l`))
