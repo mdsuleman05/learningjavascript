dc143bc93d720d3a37cd53f31f10191af692e7eb
mdsuleman05 <165216762+mdsuleman05@users.noreply.github.com>
2024-03-28 09:59:35 +0000

    mmm

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,6 @@
+const name="suleman";
+const repoCount=50;
+console.log(name+ repoCount+"value");
+console.log(`hello my name is ${name} and my repocount is ${repoCount}`)
+
+const gameName = new String(`suleman`)
