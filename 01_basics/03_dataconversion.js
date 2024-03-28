c9bf1add799a693d1697d064951ba5b07a81b439
mdsuleman05 <165216762+mdsuleman05@users.noreply.github.com>
2024-03-28 05:42:40 +0000

    datatypes

--- 01_basics/03_dataconversion.js
+++ 01_basics/03_dataconversion.js
@@ -1,6 +1,16 @@
-let score="33";
+/*
+let score=false;
 console.log(typeof score)
 console.log(typeof (score))
 let valueInNumber=Number(score)
 console.log(typeof valueInNumber);
 console.log(typeof (valueInNumber));
+console.log(valueInNumber);
+*/
+//outcomes
+
+//"33"=>33
+//"33abc"=>Nan
+//true=1;false=0
+
+
